5739a572609fb471879dca3fead2742a60deb40d
Hari Nair <nairgh@hotmail.com>
2024-10-24 16:04:26 -0400

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Data />
+  <DemoOne />
+  <DemoTwo />
+  <Methods />
+  <VModel />
+  <Computed />
+  <Watch />
+  <ProvideInject />
+  <LifecycleO />
+  <LifecycleC />
+  <TemplateRef />
+  <Person />
+  <ClickCounter />
+  <HoverCounter />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Data from './components/Data.vue'
+import DemoOne from './components/DemoOne.vue'
+import DemoTwo from './components/DemoTwo.vue'
+import Methods from './components/Methods.vue'
+import VModel from './components/VModel.vue'
+import Computed from './components/Computed.vue'
+import Watch from './components/Watch.vue'
+import ProvideInject from './components/ProvideInject.vue'
+import LifecycleO from './components/LifecycleO.vue'
+import LifecycleC from './components/LifecycleC.vue'
+import TemplateRef from './components/TemplateRef.vue'
+import Person from './components/Person.vue'
+import ClickCounter from './components/ClickCounter.vue'
+import HoverCounter from './components/HoverCounter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Data,
+    DemoOne,
+    DemoTwo,
+    Methods,
+    VModel,
+    Computed,
+    Watch,
+    ProvideInject,
+    LifecycleO,
+    LifecycleC,
+    TemplateRef,
+    Person,
+    ClickCounter,
+    HoverCounter,
+  },
 }
 </script>
 
---
+++ src/components/DemoOne.vue
@@ -0,0 +1,23 @@
+<template>
+  <h2>{{ name }}</h2>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'DemoOne',
+  setup() {
+    const name = ref('Hari')
+
+    setTimeout(() => {
+      name.value = 'Developer'
+    }, 2000)
+    return {
+      name,
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
